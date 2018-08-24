cdd405bfd64bb6fdcfcf88a64061204c0a65f8a7
Einlanzerous <einlanzerous@live.com>
2018-08-24 13:06:38 -0400

    Basic UI added via CSS min package. QoL adjustments to chat app

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ io.on('connection', (socket) => {
     socket.on('createMessage', (message, callback) => {
         console.log(`createMessage\n`, message);
         io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('Server response');
+        callback();
     });
 
     socket.on('createLocationMessage', (coords) => {
